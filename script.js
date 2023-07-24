bb6e6511942084436883ce5f92cf254413f241bf
steff2401 <stephank@hotmail.no>
2023-07-24 11:28:57 +0200

    Create game board and be able to render it to HTML

---
+++ script.js
@@ -0,0 +1,28 @@
+const gameBoard = (function() {
+
+    let gameBoardArray = [["O", "X", "O"],
+                          ["X", "O", "X"],
+                          ["O", "X", "X"]];
+
+
+    const renderGameBoard = () => (function(gameBoardArray) {
+
+        const gameBoardDiv = document.querySelector(".game-board");
+
+        for (let row = 0; row < gameBoardArray.length; row++) {
+
+            for (let col = 0; col < gameBoardArray[row].length; col++) {
+                const gameBoardSquare = document.createElement("div");
+                gameBoardSquare.textContent = gameBoardArray[row][col];
+                document.getElementById(row).appendChild(gameBoardSquare);
+            }
+        }
+    })(gameBoardArray);
+
+    return {renderGameBoard};
+
+})();
+
+gameBoard.renderGameBoard();
+
+
