92b1f686f705718d5f20bae462bbf65a01c33baf
steff2401 <stephank@hotmail.no>
2023-07-24 14:21:34 +0200

    Roughly finish logic for game over (still ugly code)

--- script.js
+++ script.js
@@ -87,7 +87,9 @@ const gameController = (function() {
         // 1. Whole row is same --> check that all gameBoardArray[row][0] == gameBoardArray[row][1] == gameBoardArray[row][2]
         // 2. Whole column is same --> check that gameBoardArray[0][col] == gameBoardArray[1][col] == gameBoardArray[2][col]
         // 3. Whole diagonal is same --> check that gameBoardArray[0][0] == gameBoardArray[1][1] == gameBoardArray[2][2] or that gameBoardArray[0][2] == gameBoardArray[1][1] == gameBoardArray[2][0]
-        // 4. All squares are filled and none of the above applies (a tie)  
+        // 4. All squares are filled and none of the above applies (a tie)
+        
+        let allSquaresFilled = true;
 
         for (let row = 0; row < board.length; row++) {
 
@@ -96,12 +98,31 @@ const gameController = (function() {
                 if (col == 2 && board[row][col] !== "") { // checking for 1.
 
                     if (board[row][0] == board[row][1] && board[row][1] == board[row][2]) {
-                        console.log("Won");
+                        console.log("Won, whole row is same");
+                    }   
+                }
+
+                if (row == 2 && board[row][col] !== "") { // checking for 2.
+
+                    if (board[0][col] == board[1][col] && board[1][col] == board[2][col]) {
+                        console.log("Won, whole column is same");
                     }
-                    
                 }
                 
+                if (board[row][col] == "") { // set-up for 4.
+                    allSquaresFilled = false;
+                }
             }
+        }
+
+        // checking for 3.
+        if ((board[0][0] != "" && board[0][0] == board[1][1] && board[1][1] == board[2][2]) || (board[0][2] != "" && board[0][2] == board[1][1] && board[1][1] == board[2][0])) {
+            console.log("Won, diagonal is same"); 
+        }
+
+        // checking for 4.
+        if (allSquaresFilled) {
+            console.log("Tie");
         }
     }
 
